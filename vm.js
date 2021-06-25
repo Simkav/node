01921c233cfe3dcc06ad577c4064823421f15e81
Khomenko Ivan <simkav2411@gmail.com>
2021-06-25 11:42:05 +0300

    update vm.js

--- vm.js
+++ vm.js
@@ -1,15 +1,43 @@
 const vm = require('vm')
-// eval к которому можно прибивать контекст
-// Не секьюрно
-// A common use case is to run the code in a different V8 Context.
-// This means invoked code has a different global object than the invoking code.
-const context = { a: 1, b: 2 }
-vm.createContext(context)
+/* 
+eval к которому можно прибивать контекст
+Не секьюрно
+
+Частый случай применения выполнять код в разных контекстах(eval.call())
+*/
+// const context = { a: 1, b: 2 }
+// vm.createContext(context)
 const code = 'console.log("abc")'
-vm.runInThisContext(code)
+// vm.runInThisContext(code)
 const a = new vm.Script(code)
-a.runInThisContext()
+// a.runInThisContext()
 // new vm.Script(code[, options])
 // vm.Script (Script) = class в котором прекомпиленый скрипт который в последствии можно юзать
-console.log('test');
-const c = vm.runInNewContext('console.log("abc")')
+
+// script.createCachedData()
+// Returns: <Buffer>
+// Creates a code cache that can be used with the Script constructor's cachedData option. Returns a Buffer. This method may be called at any time and any number of times.
+
+const context = {
+  animal: 'cat',
+  count: 2,
+  a: function () {
+    console.log(this.animal)
+  },
+  b: function () {
+    console.log(this.count)
+  }
+}
+
+const script = new vm.Script('count +=1; name = "kitty";')
+
+vm.createContext(context)
+const cont2 = { animal: 'sobaka', count: 35 }
+// vm.createContext(cont2)
+// for (let i = 0; i < 10; ++i) {
+//   script.runInContext(context)
+//   script.runInContext(cont2)
+// }
+const script2 = new vm.Script('this.b();this.a()')
+script2.runInContext(context)
+// console.log(context)
