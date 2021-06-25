947bc4d17eddd5aa29e5295b740f9a7e96e72a91
Khomenko Ivan <simkav2411@gmail.com>
2021-06-25 16:07:09 +0300

    update wasi

--- wasi.js
+++ wasi.js
@@ -1,12 +1,4 @@
-/*
-The WASI class provides the WASI system call API and additional convenience methods for working with WASI-based applications.
-Each WASI instance represents a distinct sandbox environment. 
-For security purposes, each WASI instance must have its command-line arguments, environment variables, and sandbox directory structure configured explicitly. 
-
-new WASI([options])
-
-*/
-
+/* 
 'use strict';
 const fs = require('fs');
 const { WASI } = require('wasi');
@@ -25,3 +17,28 @@ const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
 
   wasi.start(instance);
 })();
+ */
+/* 
+WASI = это системный интерфейс для платформы WebAssembly. 
+
+
+
+The WASI class provides the WASI system call API and additional convenience methods for working with WASI-based applications.
+Each WASI instance represents a distinct sandbox environment. 
+For security purposes, each WASI instance must have its command-line arguments, environment variables, and sandbox directory structure configured explicitly. 
+
+new WASI([options])
+options <Object>
+args <Array> An array of strings that the WebAssembly application will see as command-line arguments. 
+The first argument is the virtual path to the WASI command itself. Default: [].
+env <Object> An object similar to process.env that the WebAssembly application will see as its environment. Default: {}.
+preopens <Object> This object represents the WebAssembly application's sandbox directory structure. 
+The string keys of preopens are treated as directories within the sandbox.
+The corresponding values in preopens are the real paths to those directories on the host machine.
+returnOnExit <boolean> By default, WASI applications terminate the Node.js process via the __wasi_proc_exit() function.
+Setting this option to true causes wasi.start() to return the exit code rather than terminate the process. Default: false.
+stdin <integer> The file descriptor used as standard input in the WebAssembly application. Default: 0.
+stdout <integer> The file descriptor used as standard output in the WebAssembly application. Default: 1.
+stderr <integer> The file descriptor used as standard error in the WebAssembly application. Default: 2.
+
+*/
