053e941a7b917d42de0e7a8f25be851f091dffcd
Khomenko Ivan <simkav2411@gmail.com>
2021-07-03 17:39:43 +0300

    some upd

--- stream.js
+++ stream.js
@@ -1,27 +1,135 @@
-const { Readable } = require('stream')
+// https://cdn-media-1.freecodecamp.org/images/1*HGXpeiF5-hJrOk_8tT2jFA.png
+/* 
+Потоки позволяют обмениваться данными небольшими частями, что в свою очередь дает возможность в своей работе не расходовать много памяти.
+ 
+Конечно, это зависит от того, как вы реализуется внутренний функционал потока.
 
-class Counter extends Readable {
-  constructor (opt) {
-    super(opt)
 
-    this._max = 30
-    this._index = 0
+Виды потоков
+
+Есть четыре вида потоков:
+
+Readable — поток, который предоставляет данные на чтение;
+Writable — поток, в который данные можно записывать;
+Duplex — поток, из которого можно как читать данные (Readable), 
+так и записывать в него (Writable), при этом процесс чтения и записи просиходит независимо друго от друга;
+Transform — разновидность Duplex потоков, которые могут изменять данные при их записи и чтении в/из потока 
+(чаще используется как промежуточное звено в цепочке передачи данных).
+
+
+
+Потоки хранят данные в своем внутреннем буфере.
+
+Размер буфера можно указать через параметр highWaterMark, который можно задать в конструкторе класса.
+
+
+В Readable потоке данные буферизируются, когда над ним вызвается метод push(data),
+и остаются в буфере до тех пор, пока их не прочитают, вызвав метод read().
+Как только общий размер внутреннего буфера Readable потока достигнет порогового значения,
+указанного в highWaterMark, поток временно прекратит чтение данных.
+
+Для Writable буферизация происходит во время вызова над ним метода write(data).
+Метод вернет true, пока размер буфера не достиг значения highWaterMark, и false, когда буфер переполнен.
+
+При использовании метода pipe(), как раз в этот момент он «останавливает» чтение данных,
+ожидает событие «drain», после чего передача данных возобновляется.
+
+*/
+
+// const fs = require('fs')
+// const read = fs.createReadStream('./testfile')
+// const write = fs.createWriteStream('./testfile', { flags: 'a' })
+// read.pipe(write)
+// console.log('aboba')
+// const r = fs.createReadStream('./test')
+// const w = fs.createWriteStream(',/test', { flags: 'a+' })
+// r.on('open', data => {
+//   console.log(data)
+//   r.pipe(w)
+// })
+
+/* let l = 0
+console.time('a')
+const read = fs.createReadStream('./test') // { highWaterMark: 1 })
+const write = fs.createWriteStream('./test', { flags: 'a' })
+read.on('data', data => {
+  l++
+  if (l > 50000) {
+    console.log('limit')
+    read.destroy()
   }
-
-  _read () {
-    this._index += 1
-
-    if (this._index > this._max) {
-      this.push(null)
-    } else {
-      const buf = Buffer.from(`${this._index}`, 'utf8')
-      console.log(`Added: ${this._index}. Could be added? `, this.push(buf))
-    }
-  }
-}
-
-const counter = new Counter({ highWaterMark: 2 })
-
-counter.on('data', chunk => {
-  console.log(chunk)
 })
+read.pipe(write)
+read.on('close', () => {
+  console.timeEnd('a')
+}) */
+/* let i = 0
+let counter = 0
+const serv = http.createServer((req, res) => {
+  const read = fs.createReadStream('./test', { highWaterMark: 15 })
+  const a = read.read(25)
+  console.log(a)
+  read.pipe(res)
+})
+
+serv.listen(8001)
+ */
+
+/* const { Writable } = require('stream')
+
+const outStream = new Writable({
+  write (chunk, encoding, callback) {
+    console.log(chunk.toString())
+    callback()
+  }
+})
+
+process.stdin.pipe(outStream) */
+
+/* const { Readable } = require('stream');
+
+const inStream = new Readable({
+  read() {}
+});
+
+inStream.push('ABCDEFGHIJKLM');
+inStream.push('NOPQRSTUVWXYZ');
+
+inStream.push(null); // No more data
+
+inStream.pipe(process.stdout); */
+
+const { createReadStream, createWriteStream } = require('fs')
+// const read = createReadStream('./test')
+// const write = createWriteStream('./test.copy')
+
+// read.pipe(write).on('error', console.error)
+
+/* const write = createWriteStream('./input')
+process.stdin.pipe(write) */
+
+/* const { PassThrough } = require('stream')
+
+const report = new PassThrough()
+
+read.pipe(report).pipe(write)
+
+report.on('data', data => {
+  console.log(data)
+})
+ */
+
+const http = require('http')
+const serv = http.createServer((req, res) => {
+  res.write('aboba')
+  process.stdin.on('data', data => {
+    if (data.toString() === 'exit\n') {
+      res.end()
+      return
+    }
+    res.write(data)
+  })
+  res.on('close', () => {})
+})
+
+serv.listen(8001)
